ec5bb2a1768a3422bd2e3dd4b3d652f6623da3ec
Aditya Gupta <ag15035@gmail.com>
2022-11-27 11:40:56 +0530

    feat: add ~/backup 'partition'

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -25,6 +25,13 @@
       options = [ "subvol=home" ];
     };
 
+  fileSystems."/home/adityag/backup" = 
+    {
+      device = "/dev/disk/by-uuid/0c3283d1-aa4d-4b0e-8ae3-e0f65aba7d6a";
+      fsType = "btrfs";
+      options = [ "compress=zstd:9" "noatime" "subvol=backup" ];
+    };
+
   fileSystems."/home/adityag/projects" =
     { device = "/dev/disk/by-uuid/1bcea90d-7cba-4aa8-91fe-ccd177d1ce79";
       fsType = "btrfs";

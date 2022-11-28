8ae703683a9fef8720ff15d51202da38323efc21
Aditya Gupta <ag15035@gmail.com>
2022-11-28 14:46:12 +0530

    fix: unbound config

--- system/unbound.nix
+++ system/unbound.nix
@@ -12,12 +12,12 @@ let
       repo = "hosts";
       rev = "3.0.0";
       sha256 = "01g6pc9s1ah2w1cbf6bvi424762hkbpbgja9585a0w99cq0n6bxv";
-    } + "/alternates/fakenews-porn/hosts");
+    } + "/alternates/fakenews-gambling-porn/hosts");
 
     phases = [ "installPhase" ];
 
     installPhase = ''
-      ${pkgs.gawk}/bin/awk '{sub(/r$/,"")} {sub(/^127.0.0.1/,"0.0.0.0")} BEGIN { OFS = "" } NF == 2 && $1 == "0.0.0.0" { print "local-zone: "", $2, "" static"}' $src | tr '[:upper:]' '[:lower:]' | sort -u >  $out
+      ${pkgs.gawk}/bin/awk '{sub(/r$/,"")} {sub(/^127.0.0.1/,"0.0.0.0")} BEGIN { OFS = "" } NF == 2 && $1 == "0.0.0.0" { print "local-zone: ", $2, " static"}' $src | tr '[:upper:]' '[:lower:]' | sort -u >  $out
     '';
 
   };
@@ -30,17 +30,20 @@ in {
 
   services.unbound = {
     enable = true;
-    #allowedAccess = [ "127.0.0.0/24" "192.168.0.0/24" ]; # update for your LAN config
-    interfaces = [ "0.0.0.0" ];
-    forwardAddresses =
-      [ "1.0.0.1@853#cloudflare-dns.com" "1.1.1.1@853#cloudflare-dns.com" ];
-    extraConfig = ''
-        so-reuseport: yes
-        tls-cert-bundle: /etc/ssl/certs/ca-certificates.crt
-        tls-upstream: yes
-
-      include: "${adblockLocalZones}"
-        '';
+    settings = {
+      forward-zone = [{
+	name = ".";
+        forward-addr = [ "1.0.0.1@853#cloudflare-dns.com" "1.1.1.1@853#cloudflare-dns.com" ];
+      }];
+      server = {
+	access-control = [ "127.0.0.0/24 allow" "192.168.0.0/24 allow" ]; # update for your LAN config
+      	so-reuseport = true;
+      	tls-cert-bundle = "/etc/ssl/certs/ca-certificates.crt";
+      	tls-upstream = true;
+	include = "${adblockLocalZones}";
+      };
+    };
   };
-
 }
+
+# ex: shiftwidth=2 expandtab:

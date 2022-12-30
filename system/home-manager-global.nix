76aa480a7475f5eaf576089774dda27551817ad6
Aditya Gupta <ag15035@gmail.com>
2022-12-30 14:09:54 +0530

    fix: home-manager.users.adityag.home.stateVersion not defined

--- system/home-manager-global.nix
+++ system/home-manager-global.nix
@@ -11,4 +11,7 @@
   # install to /etc/profiles instead of $HOME/.nix-profile
   # pata nhi kaise help krega, lekin future me default hoga ye
   home-manager.useUserPackages = true;
+
+  # error aa rha tha iske bina, jb nix-channel --remove phir --add,--update rebuild krne pe
+  home-manager.users.adityag.home.stateVersion = "22.05";
 }
